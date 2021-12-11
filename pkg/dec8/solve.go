5f9f7cc974fe4e8bdc4ba001610d088f3649f70f
Tomas Aschan <1550920+tomasaschan@users.noreply.github.com>
2021-12-11 11:49:00 +0100

    Tidy up and simplify

--- pkg/dec8/solve.go
+++ pkg/dec8/solve.go
@@ -46,57 +46,97 @@ func B(input string) int {
 }
 
 func outputValue(display [][]string) int {
-	mapping := mapWires(append(display[0], display[1]...))
+	wiring := mapWires(append(display[0], display[1]...))
 
 	value := 0
 	for i, s := range display[1] {
-		value += int(math.Pow(10, float64(3-i))) * mapping.lookup(s)
+		value += int(math.Pow(10, float64(3-i))) * wiring[s]
 	}
 
 	return value
 }
 
-func mapWires(patterns []string) knowledge {
-	k := tabulaRasa()
+func mapWires(patterns []string) map[string]int {
+	var (
+		one, two, three, four, five, six, seven, eight, nine, zero string
+		twos, threes, fives, sixes, nines, zeroes                  map[string]bool = map[string]bool{}, map[string]bool{}, map[string]bool{}, map[string]bool{}, map[string]bool{}, map[string]bool{}
+	)
 
-	// first pass: eliminate impossible combos
-	for _, wires := range patterns {
-		switch len(wires) {
+	// first pass: group wire patterns by number of wires
+	for _, pattern := range patterns {
+		switch len(pattern) {
 		case 2:
-			k.learn(wires, "cf", 1)
+			one = pattern
 		case 3:
-			k.learn(wires, "acf", 7)
+			seven = pattern
 		case 4:
-			k.learn(wires, "bcdf", 4)
+			four = pattern
 		case 5:
-			k.learn(wires, "abcdefg", 2, 3, 5)
+			twos[pattern] = true
+			threes[pattern] = true
+			fives[pattern] = true
 		case 6:
-			k.learn(wires, "abcdefg", 6, 9, 0)
+			sixes[pattern] = true
+			nines[pattern] = true
+			zeroes[pattern] = true
 		case 7:
-			k.learn(wires, "abcdefg", 8)
+			eight = pattern
 		}
 	}
-	// after the first pass, 1, 4, 7, and 8 are disambiguated
-
-	// second pass: use more knowledge about all digits
-
-	// out of 2, 3 and 5, only 3 includes all wires from 1
-	k.disambiguate(1, 3, true)
-
-	// out of 6, 9 and 0, only 6 does _not_ include all wires from 1
-	k.disambiguate(1, 6, false)
-
-	// out of 9 and 0, only 9 includes all wires from 4
-	k.disambiguate(4, 9, true)
-
-	// out of 2 and 5, 6 only contains 5
-	six := singleton(k.digits[6])
-	for candidate := range k.digits[5] {
-		if utils.ContainsAll(six, candidate) {
-			k.learn(candidate, candidate, 5)
-			break
+	// out of 6, 9 and 0, only 9 covers 4
+	for p := range sixes {
+		if utils.ContainsAll(p, four) {
+			nine = p
+			delete(sixes, p)
+			delete(zeroes, p)
 		}
 	}
 
-	return k
+	// out of 6, and 0, only 0 covers 1
+	for p := range zeroes {
+		if utils.ContainsAll(p, one) {
+			zero = p
+			delete(sixes, p)
+		}
+	}
+
+	// now, 6 has only one option
+	for p := range sixes {
+		six = p
+	}
+
+	// out of 2, 3 and 5, only 3 covers 1
+	for p := range threes {
+		if utils.ContainsAll(p, one) {
+			three = p
+			delete(twos, p)
+			delete(fives, p)
+		}
+	}
+
+	// out of 2 and 5, only 5 is covered by 6
+	for p := range fives {
+		if utils.ContainsAll(six, p) {
+			five = p
+			delete(twos, p)
+		}
+	}
+
+	// now, 2 only has one option
+	for p := range twos {
+		two = p
+	}
+
+	return map[string]int{
+		zero:  0,
+		one:   1,
+		two:   2,
+		three: 3,
+		four:  4,
+		five:  5,
+		six:   6,
+		seven: 7,
+		eight: 8,
+		nine:  9,
+	}
 }
